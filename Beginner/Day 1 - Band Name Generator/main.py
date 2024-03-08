b217994520c6500bc6c7a4a5ed6d97d783ee325e
thenjiiiwe <thenjiiiwe27@gmail.com>
2024-03-08 15:03:41 +0200

    First commit - Day 1 complete

---
+++ Beginner/Day 1 - Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+fruit = input("What's your favourite fruit?\n")
+colour = input("What's your favourite colour?\n")
+print("Your band name could be " + colour + " " + fruit)
